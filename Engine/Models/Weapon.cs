15c88ae24b074b31a18ccf772dc0af5380f3e208
Kaitlyn Vuong <khanhlinhvuongklv@gmail.com>
2025-01-05 17:53:42 -0500

    Created GameItem and Weapon class in Models, and ItemFactory class in Factories

---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MininmumDamage {  get; set; }
+        public int MaximumDamage { get; set; }
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) : base(itemTypeID, name, price) 
+        {
+            MininmumDamage = minDamage;
+            MaximumDamage = maxDamage;
+        }
+        
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MininmumDamage, MaximumDamage);
+        }
+    }
+}
